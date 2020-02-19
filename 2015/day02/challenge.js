6cb9d3316f610a2275906ced545adb9b19529c0e
abonmassip <abonmassip@gmail.com>
2020-02-19 08:49:12 -0800

    up

--- 2015/day02/challenge.js
+++ 2015/day02/challenge.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 
-fs.readFile('./input.txt', (err, data) => {
+fs.readFile('./myinput.txt', (err, data) => {
   console.time('challenge')
 
   if (err) {
     console.log('error!!!!!');
   }
 
-  var array = data.toString().split("\n");
+  var array = data.toString().split(/\r?\n/g);
+  array.map(num => )
   console.log(array);
 
 })
