fbeacbe203298843259a2114891e1b29284e00ad
abonmassip <abonmassip@gmail.com>
2020-02-20 22:55:32 -0800

    day2

--- 2015/day02/challenge.js
+++ 2015/day02/challenge.js
@@ -1,14 +1,27 @@
 const fs = require('fs');
 
-fs.readFile('./myinput.txt', (err, data) => {
+fs.readFile('./input.txt', (err, data) => {
   console.time('challenge')
 
   if (err) {
     console.log('error!!!!!');
   }
-
   var array = data.toString().split(/\r?\n/g);
-  array.map(num => )
-  console.log(array);
+  let totalSquareFeet = 0;
+  let totalRibbon = 0;
+
+  array.map(size => {
+    if (size !== '') {
+      const sidesArray = size.split("x").sort(function(a, b){return a-b});
+      sides = sidesArray.map(value => Number(value));
+      const package = sides[0]*sides[1] * 3 + (sides[0]*sides[2] + sides[1]*sides[2]) * 2;
+      totalSquareFeet += package;
+      const ribbon = (sides[0]+sides[1])*2 + (sides[0]*sides[1]*sides[2]);
+      totalRibbon += ribbon;
+    }
+  });
+  console.timeEnd('challenge');
+
+  console.log(`they need a total of ${totalSquareFeet} square feet of wrapping paper and ${totalRibbon} feet of ribbon`);
 
 })
