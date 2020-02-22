2f8cae69199e7368b4c715d4ab2c85fa1662b653
abonmassip <abonmassip@gmail.com>
2020-02-21 23:11:47 -0800

    day3 part2

--- 2015/day03/challenge.js
+++ 2015/day03/challenge.js
@@ -9,36 +9,55 @@ fs.readFile('./input.txt', (err, data) => {
 
   var dataArray = data.toString().split('');
 
-  let x = 0;
-  let y = 0;
-  let positionTrace = ['0,0']
+  let position = [0,0];
+  let positionTrace = ['0,0'];
+  let positionSanta = [0,0];
+  let positionRobot = [0,0];
+  let positionDuoTrace = ['0,0'];
 
-  dataArray.map(position => {
-    switch(position) {
+  function arrayToString(array){
+    return `${array[0]},${array[1]}`
+  }
+
+  dataArray.forEach((pos, i) => {
+
+    function changePosition (axis, mov) {
+      position[axis] += mov;
+      (i%2 === 0) ? (positionSanta[axis] += mov) : (positionRobot[axis] += mov);
+    }
+
+    switch(pos) {
       case '>':
-        x += 1;
+        changePosition(0, 1);
         break;
       case 'v':
-        y -= 1;
+        changePosition(1, -1);
         break;
       case '<':
-        x -= 1;
+        changePosition(0, -1);
         break;
       case '^':
-        y += 1;
+        changePosition(1, 1);
         break;
       default:
         break;
     }
-
-    if(!positionTrace.includes(`${x},${y}`)) {
-      positionTrace.push(`${x},${y}`);
+    
+    if(!positionTrace.includes(arrayToString(position))) {
+      positionTrace.push(arrayToString(position));
+    }
+    if(!positionDuoTrace.includes(arrayToString(positionSanta))) {
+      positionDuoTrace.push(arrayToString(positionSanta));
+    }
+    if(!positionDuoTrace.includes(arrayToString(positionRobot))) {
+      positionDuoTrace.push(arrayToString(positionRobot));
     }
 
   })
 
   console.timeEnd('challenge');
 
-  console.log(`${positionTrace.length} houses receiveat least one present`)
+  console.log(`part one: ${positionTrace.length} houses received at least one present`)
+  console.log(`part two: ${positionDuoTrace.length} houses received at least one present`)
 
 })
