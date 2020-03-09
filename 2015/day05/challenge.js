e5d3644f254cbbfccc76c5639f20e86a6f1a7249
Albert <abonmassip@gmail.com>
2020-03-08 17:10:55 -0700

    day5 part2

--- 2015/day05/challenge.js
+++ 2015/day05/challenge.js
@@ -5,8 +5,8 @@ fs.readFile('./input.txt', (err, data) => {
 
   const input = data.toString();
   const inputArray = input.split(/\r?\n/g);
-  console.log(inputArray);
   let niceStrings = 0;
+  let niceStringsTwo = 0;
 
   const vowels = ['a', 'e', 'i', 'o', 'u'];
   const forbidden = ['ab', 'cd', 'pq', 'xy'];
@@ -17,34 +17,59 @@ fs.readFile('./input.txt', (err, data) => {
   function findRepeatedLetter(char, i, array) {
     return  char === array[i+1];
   }
-  function findForbiddenString(string, i, array) {
-    const doubleString = `${string}${array[i+1]}`
-    return forbidden.find(forbiddenItem => doubleString === forbiddenItem);
+  function findForbiddenString(char, i, array) {
+    const pair = `${char}${array[i+1]}`
+    return forbidden.find(forbiddenItem => pair === forbiddenItem);
+  }
+  function findRepeatedPair(char, i, array) {
+    const pair = `${char}${array[i+1]}`
+    const otherChars = [...array];
+    otherChars[i] = null;
+    otherChars[i+1] = null;
+    return otherChars.find((otherChar, index, newArray) => {
+      newPair = `${otherChar}${newArray[index+1]}`;
+      return newPair === pair;
+    })
+  }
+  function findRepeatedLetterWithJump(char, i, array) {
+    return  char === array[i+2];
   }
 
   inputArray.forEach((singleString) => {
+    const charsArray = singleString.split('');
     let howManyVowels = 0;
-    let repeatedLetter = false;
-    let containsForbiddenStrings = false;
 
     // count vowels
     singleString.split('').forEach(char => {
       isAVowel(char) ? howManyVowels++ : null;
     });
-    // console.log('vowels: ', howManyVowels);
+
     // contains repeated letter?
-    repeatedLetter = !!singleString.split('').find(findRepeatedLetter);
-    // console.log('double? ', repeatedLetter);
+    const hasRepeatedLetter = !!charsArray.find(findRepeatedLetter);
+    
     // NOT contains 'ab', 'cd', 'pq', 'xy'?
-    containsForbiddenStrings = !!singleString.split('').find(findForbiddenString);
-    // console.log('forbidden? ', containsForbiddenStrings);
-    // then add 1 niceString
-    if(howManyVowels > 2 && repeatedLetter === true && containsForbiddenStrings === false){
+    const hasForbiddenStrings = !!charsArray.find(findForbiddenString);
+    
+    // add 1 niceString
+    if(howManyVowels > 2 && hasRepeatedLetter && !hasForbiddenStrings){
       niceStrings++;
+    }
+
+    // Part 2
+    // contains pair letters repeated twice without overlap
+    const hasRepeatedPair = !!charsArray.find(findRepeatedPair);
+
+    // contains one letter repeated with one letter in between
+    const hasRepeatedLetterJump = !!charsArray.find(findRepeatedLetterWithJump);
+    
+    // add 1 niceStringTwo
+    if(hasRepeatedLetterJump && hasRepeatedPair){
+      niceStringsTwo++;
     }
   });
   console.timeEnd('time');
 
-  console.log(`There are ${niceStrings} nice strings`);
+  console.log(`Part 1: There are ${niceStrings} nice strings`);
+  console.log(`Part 2: There are ${niceStringsTwo} nice strings`);
 })
 
